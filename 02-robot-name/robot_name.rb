064f1fa44ef0300d1541e931c16f71cec568bef7
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-08-26 14:07:05 -0700

    solved robot_name

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,19 @@
+class Robot
+  attr_accessor :name
 
+  def initialize
+    @name = generate_name
+  end
+
+  def generate_random
+    robot_name = ""
+    robot_name += ("A".."Z").to_a.sample(2).join
+    robot_name += ("1".."9").to_a.sample(3).join
+  end
+
+  #Why can't I remove @ from the name variable here?
+  def reset
+    @name = generate_random
+  end
+
+end
